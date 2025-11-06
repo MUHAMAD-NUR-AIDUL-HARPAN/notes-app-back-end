c355cedfc080130518ff0d19f715666fda7fd8df
Harpan <muhamadnurharpan>
2025-11-06 10:59:51 +0700

    restructure with hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,21 +1,28 @@
-const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
-
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+ 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: "localhost",
+    port: 3000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
-      // <-- TAMBAHKAN BAGIAN INI
       cors: {
-        origin: ["https://notesapp-v1.dicodingacademy.com"], // Izinkan domain frontend
+        origin: ['*'],
       },
     },
   });
-
-  server.route(routes);
+ 
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
+ 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
+ 
 init();
