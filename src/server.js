8c2dad64e4660592be6834cdd7ecb7e2d2b40693
Harpan <muhamadnurharpan>
2025-11-24 07:03:55 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,11 +1,75 @@
+// require("dotenv").config();
+
+// const Hapi = require("@hapi/hapi");
+// const notes = require("./api/notes");
+// const NotesService = require("./services/postgres/NotesService");
+// const NotesValidator = require("./validator/notes");
+
+// // users
+// const users = require("./api/users");
+// const UsersService = require("./services/postgres/UsersService");
+// const UsersValidator = require("./validator/users");
+
+// const init = async () => {
+//   const notesService = new NotesService();
+//   const usersService = new UsersService();
+
+//   await server.register([
+//     {
+//       plugin: notes,
+//       options: {
+//         service: notesService,
+//         validator: NotesValidator,
+//       },
+//     },
+//     {
+//       plugin: users,
+//       options: {
+//         service: usersService,
+//         validator: UsersValidator,
+//       },
+//     },
+//   ]);
+
+//   const server = Hapi.server({
+//     port: process.env.PORT,
+//     host: process.env.HOST,
+//     routes: {
+//       cors: {
+//         origin: ["*"],
+//       },
+//     },
+//   });
+
+//   await server.register({
+//     plugin: notes,
+//     options: {
+//       service: notesService,
+//       validator: NotesValidator,
+//     },
+//   });
+
+//   await server.start();
+//   console.log(`Server berjalan pada ${server.info.uri}`);
+// };
+
+// init();
+
+require("dotenv").config();
+
 const Hapi = require("@hapi/hapi");
+// NOTES
 const notes = require("./api/notes");
 const NotesService = require("./services/postgres/NotesService");
 const NotesValidator = require("./validator/notes");
-require("dotenv").config();
+
+// USERS
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
 
 const init = async () => {
-  const notesService = new NotesService();
+  // [PERBAIKAN 1]: Deklarasikan dan inisialisasi server terlebih dahulu
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -16,17 +80,29 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  const notesService = new NotesService();
+  const usersService = new UsersService();
+
+  // [PERBAIKAN 2]: Hapus blok server.register yang berulang (hanya perlu register sekali)
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
-
