8a354adcdcd167fcaed6506ac0027c6d02c02401
Harpan <muhamadnurharpan>
2025-11-26 08:07:13 +0700

     collaboration implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -65,6 +65,7 @@ class NotesHandler {
     try {
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
+      await this._service.verifyNoteAccess(id, credentialId);
 
       await this._service.verifyNoteOwner(id, credentialId);
       const note = await this._service.getNoteById(id);
@@ -101,6 +102,7 @@ class NotesHandler {
       this._validator.validateNotePayload(request.payload);
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
+      await this._service.verifyNoteAccess(id, credentialId);
       await this._service.verifyNoteOwner(id, credentialId);
       await this._service.editNoteById(id, request.payload);
 
--- src/services/postgres/AuthenticationsService.js
+++ src/services/postgres/AuthenticationsService.js
@@ -4,13 +4,25 @@ const InvariantError = require("../../exceptions/InvariantError");
 class AuthenticationsService {
   constructor() {
     this._pool = new Pool();
-  } // kode lain disembunyikan
+  }
+
+  async addRefreshToken(token) {
+    const query = {
+      text: "INSERT INTO authentications VALUES($1)",
+      values: [token],
+    };
+
+    await this._pool.query(query);
+  }
+
   async verifyRefreshToken(token) {
     const query = {
       text: "SELECT token FROM authentications WHERE token = $1",
       values: [token],
     };
+
     const result = await this._pool.query(query);
+
     if (!result.rows.length) {
       throw new InvariantError("Refresh token tidak valid");
     }
@@ -21,7 +33,9 @@ class AuthenticationsService {
       text: "DELETE FROM authentications WHERE token = $1",
       values: [token],
     };
+
     await this._pool.query(query);
   }
 }
+
 module.exports = AuthenticationsService;
